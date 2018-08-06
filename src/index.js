24955fe9585c12d68e8b3bb95072855d3a215f36
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-08-07 00:22:32 +0300

    Redux was added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const defaultState = {};
+
+const store = createStore(
+  rootReducer,
+  defaultState,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
