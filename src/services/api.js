2deeba455cd2466d1f854f3226a7a32f1822d668
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-09-28 02:54:23 +0300

    Changes in api service functions

--- src/services/api.js
+++ src/services/api.js
@@ -5,9 +5,37 @@ const backendHost = config.get('backendHost');
 
 const createUrl = endPoint => `${backendHost}${endPoint}`;
 
-const post = (endPoint, data, headers = {}) => axios.post(createUrl(endPoint), data, { headers });
+const getAuthorizationHeaderObject = () => {
+  const token = localStorage.getItem('token');
+
+  return token ? { Authorization: token } : {};
+};
+
+function addAuthorizationHeader(headers) {
+  if (typeof headers === 'object') {
+    return {
+      ...headers,
+      ...getAuthorizationHeaderObject(),
+    };
+  }
+
+  return getAuthorizationHeaderObject();
+}
+
+const get = (endPoint, params, headers = {}) => {
+  const url = createUrl(endPoint);
+  const newHeaders = addAuthorizationHeader(headers);
+  return axios.get(url, { params, headers: newHeaders });
+};
+
+const post = (endPoint, data, headers = {}) => {
+  const url = createUrl(endPoint);
+  const newHeaders = addAuthorizationHeader(headers);
+  return axios.post(url, data, { headers: newHeaders });
+};
 
 export {
+  get,
   post,
 
   // For tests
