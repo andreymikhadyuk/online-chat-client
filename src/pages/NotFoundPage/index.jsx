25a3591aa58274e976955a077566f6a8a9ad4bc4
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-09-27 23:24:04 +0300

    Page not found component was changed

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -3,9 +3,11 @@ import './styles.scss';
 
 const NotFoundPage = () => (
   <div className="not-found-page">
-    <span className="not-found-page__404">404</span>
-    <span className="not-found-page__the-page">The page</span>
-    <span className="not-found-page__was-not-found">was not found</span>
+    <div className="not-found-page__wrapper">
+      <span className="not-found-page__404">404</span>
+      <span className="not-found-page__the-page">The page</span>
+      <span className="not-found-page__was-not-found">was not found</span>
+    </div>
   </div>
 );
 
