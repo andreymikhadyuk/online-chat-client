2f92275d647de67c4f611d9923b4f14c19eb4cac
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-09-27 22:47:34 +0300

    Routing was added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { LoginPage, ChatPage, NotFoundPage } from '../pages';
 
 const App = () => (
-  <div>
-    <NotFoundPage />
-  </div>
+  <Switch>
+    <Route path="/login" component={LoginPage} />
+    <Route path="/chat" component={ChatPage} />
+    <Route component={NotFoundPage} />
+  </Switch>
 );
 
 export default App;
