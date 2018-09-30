59fbef1544e7097725a8ed03ae41198c306dbd45
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-10-01 00:48:06 +0300

    Message's alignment depends on whether is was from active user or not

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './styles.scss';
 
-const Message = ({ user, text }) => (
+const Message = ({ user, text, isFromActiveUser }) => (
   <section className="message">
-    <div className="message__meta">
+    <div className={classNames('message__meta', { message__meta_right: isFromActiveUser })}>
       <span className="message__user">{user}</span>
     </div>
-    <div className="message__content">
-      <span className="message__text">{text}</span>
+    <div className={classNames('message__content', { message__content_right: isFromActiveUser })}>
+      <span className={classNames('message__text', { message__text_right: isFromActiveUser })}>
+        {text}
+      </span>
     </div>
   </section>
 );
@@ -16,11 +19,13 @@ const Message = ({ user, text }) => (
 Message.propTypes = {
   user: PropTypes.string,
   text: PropTypes.string,
+  isFromActiveUser: PropTypes.bool,
 };
 
 Message.defaultProps = {
   user: 'Anonymous',
   text: 'Empty message',
+  isFromActiveUser: false,
 };
 
 export default Message;
