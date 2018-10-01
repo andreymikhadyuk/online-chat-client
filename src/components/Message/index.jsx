4d9a4c669fa9d44546addd67120cf80fa58d8678
Andrei Mikhadziuk <mihadiuk.andrej@gmail.com>
2018-10-01 12:25:54 +0300

    Display created time of message next to username

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import moment from 'moment';
+import { MESSAGE_DATE_FORMAT } from 'consts/formats';
 import './styles.scss';
 
-const Message = ({ user, text, isFromActiveUser }) => (
+const Message = ({ user, text, createdAt, isFromActiveUser }) => (
   <section className="message">
     <div className={classNames('message__meta', { message__meta_right: isFromActiveUser })}>
       <span className="message__user">{user.username}</span>
+      <span className="message__time">{moment(createdAt).format(MESSAGE_DATE_FORMAT)}</span>
     </div>
     <div className={classNames('message__content', { message__content_right: isFromActiveUser })}>
       <span className={classNames('message__text', { message__text_right: isFromActiveUser })}>
@@ -19,6 +22,7 @@ const Message = ({ user, text, isFromActiveUser }) => (
 Message.propTypes = {
   user: PropTypes.object,
   text: PropTypes.string,
+  createdAt: PropTypes.number.isRequired,
   isFromActiveUser: PropTypes.bool,
 };
 
